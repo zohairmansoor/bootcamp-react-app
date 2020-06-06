e33e9f053d23f2d5a020749436bed303ef5fe2b9
Zohair <zohair.mansoor@gmail.com>
2020-06-06 15:51:49 +0500

    Update App.js
    Deleted Edit <code>src/App.js</code> and save to reload.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World Form Zohair Mansoor Edit <code>src/App.js</code> and save to reload.
+          Hello World Form Zohair Mansoor.
         </p>
         <a
           className="App-link"
